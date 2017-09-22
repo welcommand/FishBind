ac7acb868dd4e3f12140e39a7bdeb3bcf491d7ce
WELCommand <ios_programming@163.com>
2017-09-22 10:37:12 +0800

    change public API

--- FishBind/IIFishBind.h
+++ FishBind/IIFishBind.h
@@ -12,19 +12,27 @@ typedef void(^IIFishMindine) (id object, NSString *keyPatch,id resule,NSArray *a
 
 @interface IIFish : NSObject
 @property (nonatomic, assign) id object;
-@property (nonatomic, assign) SEL key;
+@property (nonatomic, copy) NSString *property;
+@property (nonatomic, copy) NSString *selector;
 @property (nonatomic, copy) id callBack;
 
-+ (instancetype)fish:(id)object key:(SEL)key callBack:(IIFishMindine)callBack;
++ (instancetype)fish:(id)object selector:(SEL)selector callBack:(IIFishMindine)callBack;
++ (instancetype)fish:(id)object property:(NSString*)property;
+
+
 @end
 
 @interface IIPostFish : IIFish
-+ (instancetype)fish:(id)object key:(SEL)key;
-+ (instancetype)fish:(id)blockObject;
+
++ (instancetype)postBlock:(id)blockObject;
++ (instancetype)post:(id)object property:(NSString *)property;
++ (instancetype)post:(id)object selector:(SEL)selector;
+
 @end
 
 @interface IIObserverFish : IIFish
-+ (instancetype)fish:(id)object callBack:(IIFishMindine)callBack;
++ (instancetype)observer:(id)object property:(NSString *)property;
++ (instancetype)observer:(id)object callBack:(IIFishMindine)callBack;
 @end
 
 
