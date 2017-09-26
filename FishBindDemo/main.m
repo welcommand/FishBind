9d9aeda873721d755aa71391cb2cd94b2b6f2b3d
WELCommand <ios_programming@163.com>
2017-09-26 19:04:27 +0800

    add demo

--- FishBindDemo/main.m
+++ FishBindDemo/main.m
@@ -17,20 +17,39 @@ int main(int argc, const char * argv[]) {
         
         IITestObjectA *objectA = [IITestObjectA new];
         IITestObjectB *objectB = [IITestObjectB new];
+        IITestObjectA *objectA1 = [IITestObjectA new];
+        
+        objectA1.name = @"first name";
         
         [IIFishBind bindFishes:@[
                                  [IIFish both:objectA property:@"name"],
                                  [IIFish both:objectB property:@"nameData"]
                                  ]];
-        
         objectA.name = @"dead fish";
-        // put ===dead fish ===dead fish===
         NSLog(@"===%@ ===%@===",objectA.name, objectB.nameData);
-        
+        // put ===dead fish ===dead fish===
         
         objectB.nameData = @"name data";
-        //put ===name data ===name data===
         NSLog(@"===%@ ===%@===",objectA.name, objectB.nameData);
+        //put ===name data ===name data===
+        
+        [IIFishBind bindFishes:@[
+                                 [IIFish post:objectA selector:@selector(loadDataWithName:age:)],
+                                 [IIFish observer:objectB
+                                         callBack:^(IIFishCallBack *callBack, id deadFish) {
+                                             NSArray *args = callBack.args;
+                                             NSString *name = [NSString stringWithFormat:@"NAME : %@",args[0]];
+                                             // don`t call [objectB setNameData:args[0]]. will Dead loop
+                                             [deadFish setNameData:name];
+                                         }]
+                                 ]];
+        
+        [objectA loadDataWithName:@"test" age:18];
+        
+        NSLog(@"===%@ ===%@===",objectA.name, objectB.nameData);
+        //put ===test ===NAME : test===
+        NSLog(@"====%@",objectA1.name);
+        //put ====object1
         
     }
     return 0;
